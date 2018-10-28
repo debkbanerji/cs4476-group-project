5ba3e0fc83317fac61fda1c1f65017e3c6895b82
deb <debkbanerji@gmail.com>
2018-10-28 00:44:21 -0400

    Update seam carving test code

--- seamCarving/tester.py
+++ seamCarving/tester.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 
 from seamCarving import resize
 
-input_im_filename = 'black-shirt.jpg'
-# input_im_filename = 'inputSeamCarvingPrague.jpg'
+input_im_filename = 'resize-test-shirt.jpg'
 
 input_image = np.array(Image.open(input_im_filename))
 
