2990e42e561f327b6bd37d90a0b1df24c0d0ff1a
Christine Hsieh <chsieh40@gatech.edu>
2018-10-29 10:04:23 -0400

    Add resizeShirt() in seamCarving/tester to calculate what width must be added to the shirt image so it has similar aspect ratio to the body image

--- seamCarving/tester.py
+++ seamCarving/tester.py
@@ -4,21 +4,75 @@ import matplotlib.pyplot as plt
 
 from seamCarving import resize
 
-input_im_filename = 'resize-test-shirt.jpg'
+def testResize():
 
-input_image = np.array(Image.open(input_im_filename))
+    input_im_filename = 'resize-test-shirt.jpg'
 
-plt.imshow(input_image)
-plt.show()
+    input_image = np.array(Image.open(input_im_filename))
 
-output_image = resize(input_image, (input_image.shape[0] - 100, input_image.shape[1]), backgroundPixel=(255, 255, 255))
+    plt.imshow(input_image)
+    plt.show()
 
-f, subplot = plt.subplots(1, 2)
+    output_image = resize(input_image, (input_image.shape[0] - 100, input_image.shape[1]), backgroundPixel=(255, 255, 255))
 
-subplot[0].imshow(input_image)
-subplot[0].set_title('Input Image: (' + str(input_image.shape[0]) + ', ' + str(input_image.shape[1]) + ')')
+    f, subplot = plt.subplots(1, 2)
 
-subplot[1].imshow(output_image)
-subplot[1].set_title('Resized Image: (' + str(output_image.shape[0]) + ', ' + str(output_image.shape[1]) + ')')
+    subplot[0].imshow(input_image)
+    subplot[0].set_title('Input Image: (' + str(input_image.shape[0]) + ', ' + str(input_image.shape[1]) + ')')
 
-plt.show()
+    subplot[1].imshow(output_image)
+    subplot[1].set_title('Resized Image: (' + str(output_image.shape[0]) + ', ' + str(output_image.shape[1]) + ')')
+
+    plt.show()
+
+def resizeShirt():
+
+    shirt_torsoPoints = np.load('../pipelining/t_shirt1torsoPoints.npy')
+    body_torsoPoints = np.load('../pipelining/torsoPoints.npy')
+
+    input_im_filename = 'resize-test-shirt.jpg'
+    input_image = np.array(Image.open(input_im_filename))
+
+    shirt_highestPoint = (shirt_torsoPoints[0,1] if shirt_torsoPoints[0,1] < shirt_torsoPoints[1,1] else shirt_torsoPoints[1,1])
+    shirt_lowestPoint = (shirt_torsoPoints[4,1] if shirt_torsoPoints[4,1] > shirt_torsoPoints[5,1] else shirt_torsoPoints[5,1])
+    shirt_leftmostPoint = shirt_torsoPoints[4,0]
+    shirt_rigtmostPoint = shirt_torsoPoints[5,0]
+
+    body_highestPoint = (body_torsoPoints[0,1] if body_torsoPoints[0,1] < body_torsoPoints[1,1] else body_torsoPoints[1,1])
+    body_lowestPoint = (body_torsoPoints[4,1] if body_torsoPoints[4,1] > body_torsoPoints[5,1] else body_torsoPoints[5,1])
+    body_leftmostPoint = body_torsoPoints[4,0]
+    body_rigtmostPoint = body_torsoPoints[5,0]
+
+    shirt_width = int(shirt_rigtmostPoint - shirt_leftmostPoint) #x
+    shirt_height = int(shirt_lowestPoint - shirt_highestPoint) #y
+    body_width = int(body_rigtmostPoint - body_leftmostPoint) #a
+    body_height = int(body_lowestPoint - body_highestPoint) #b
+
+
+    print ("shirt - width:" + str(shirt_width) + " by height: " + str(shirt_height))
+    print ("body - width:" + str(body_width) + " by height: " + str(body_height))
+
+    shirt_aspectRatio = shirt_height / shirt_width #y/x
+    body_aspectRatio = body_height / body_width #b/a
+    # print(shirt_aspectRatio)
+    # print(body_aspectRatio)
+
+    # calculations for how to find necessary change in shirt width
+    # TODO: come up with better way to decide changes in x and y direction
+    #  y/(x+??) = b/a
+    #  ay = xb + ??b
+    # (ay-xb)/b  = ??
+    # (ay)/b - x = ??
+
+    deltaShirtWidth = int((body_width * shirt_height) / body_height) - shirt_width
+
+    backgroundPixel = input_image[0, input_image.shape[1]-1] # top right corner
+    output_image = resize(input_image, (input_image.shape[0], input_image.shape[1] + deltaShirtWidth), backgroundPixel=backgroundPixel)
+    plt.imsave('shirt_updatedAspectRatio.png', output_image)
+
+
+def main():
+    resizeShirt()
+
+if __name__ == "__main__":
+    main();
