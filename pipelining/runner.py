3eb14f2932d2babec95b9b6e3d2fd1b21cbd17c0
rkrishnan42 <rohithk98@gmail.com>
2018-11-28 17:23:27 -0500

    result of homography transform is now shown within SeamApp widget, can now select another t-shirt to fit

--- pipelining/runner.py
+++ pipelining/runner.py
@@ -46,6 +46,9 @@ class SeamApp(QWidget):
         self.submitButton = QPushButton('Submit')
         self.submitButton.clicked.connect(self.submitPoints)
 
+        self.addShirtButton = QPushButton('Add Another Shirt')
+        self.addShirtButton.clicked.connect(self.addAnotherShirt)
+
         self.setLayout(QVBoxLayout())
 
         # self.layout = QHBoxLayout()
@@ -106,13 +109,14 @@ class SeamApp(QWidget):
         currentImageName = self.currentImageName
 
     def addAnotherShirt(self):
-        buttonReply = QMessageBox.question(self, 'Add a Shirt', "Would you like to add another shirt?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-        if buttonReply == QMessageBox.Yes:
-            print('Yes clicked.')
-            self.addShirt()
-        else:
-            print('User is done adding shirts.')
-            return
+        self.addShirt()
+        # buttonReply = QMessageBox.question(self, 'Add a Shirt', "Would you like to add another shirt?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        # if buttonReply == QMessageBox.Yes:
+        #     print('Yes clicked.')
+        #     self.addShirt()
+        # else:
+        #     print('User is done adding shirts.')
+        #     return
 
     def addShirt(self):
         self.currentImageName = self.openImageFileDialog("Select image of your T-Shirt")
@@ -126,9 +130,172 @@ class SeamApp(QWidget):
             np.save(shirtSaveName + "Img.npy", shirtImg)
             np.save(shirtSaveName + "Mask.npy", maskWithoutCollar)
             self.saveShirtCorners(shirtCorners, shirtSaveName)
-            self.addAnotherShirt()
+            self.mapShirt(shirtImg)
+            #self.addAnotherShirt()
+
+
         # foregroundMask(misc.imread(self.currentImageName, mode='RGBA'), "t_shirt" + str(self.countShirts))
         # # self.collectCorrespondences(self.currentImageName, self.referenceWidgetList[self.currentRefImage], # "t_shirt" + str(self.countShirts) + self.referenceImageList[self.currentRefImage][1])
+
+    def mapShirt(self, shirt):
+        human = 'kirtan_img.jpg'
+        image_path = 'images/'
+
+        shirt_img = shirt
+        mask_image_name = 't_shirt' + str(self.countShirts) + 'Mask.npy'
+        shirt_foreground_img = np.asarray(np.load(mask_image_name))
+        human_img = plt.imread(image_path + human)
+
+        pts_human = np.asarray(np.load('userPoints.npy'))
+        userpoints = [pts_human[:, 0], pts_human[:, 1]]
+
+        shirt_pts_name = 't_shirt' + str(self.countShirts) + 'Points.npy'
+        pts_shirt = np.asarray(np.load(shirt_pts_name))
+        shirtpoints = [pts_shirt[:, 0], pts_shirt[:, 1]]
+
+        homography = self.computeH(shirtpoints, userpoints)
+        stiched_image, sharpened_image = self.warpImage(shirt_foreground_img, shirt_img, human_img, homography)
+
+        stichedImWidget = ImageWidget("none", "none", imageArray=stiched_image)
+        sharpenedImWidget = ImageWidget("none", "none", imageArray=sharpened_image)
+        hbox = QHBoxLayout()
+        hbox.addWidget(stichedImWidget)
+        hbox.addWidget(sharpenedImWidget)
+        sharpenedImWidget.show()
+        stichedImWidget.show()
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+        vbox.addWidget(self.addShirtButton)
+        self.deleteItems(self.layout())
+
+        # self.layout.addLayout(hbox)
+        # self.layout.addWidget(self.submitButton)
+        # self.layout.setContentMargins(0, 0, 0, 0)
+
+        # self.deleteItems(self.layout())
+
+        self.layout().addLayout(vbox)
+        self.show()
+
+
+
+
+
+    def computeH(self, t1, t2):
+        pre_matrix = []
+        for i in range(len(t1[0])):
+            x1 = t1[0][i]
+            x2 = t1[1][i]
+
+            x1_prime = t2[0][i]
+            x2_prime = t2[1][i]
+
+            transposed1 = [x1, x2, 1, 0, 0, 0, -x1_prime * x1, -x1_prime * x2, -x1_prime]
+            transposed2 = [0, 0, 0, x1, x2, 1, -x2_prime * x1, -x2_prime * x2, -x2_prime]
+            pre_matrix.append(transposed1)
+            pre_matrix.append(transposed2)
+
+        matrix = np.matrix(pre_matrix)
+
+        u, s, v = np.linalg.svd(matrix)
+        homography = np.reshape(v[-1], (3, 3))
+        return homography
+
+    def warpImage(self, inputIm, image, refIm, H):
+        homography = H
+        inverted_H = np.linalg.inv(homography)
+
+        input_maxRow = inputIm.shape[0]
+        input_maxCol = inputIm.shape[1]
+
+        BACKGROUND_PIXEL = inputIm[0][0]
+        print('background_pixel = ', BACKGROUND_PIXEL)
+
+        ## WARP ##
+        input_TOP_L = np.asarray(homography.dot(np.asarray([0, 0, 1])))
+        input_TOP_L_actual = input_TOP_L[0][0] / input_TOP_L[0][2], input_TOP_L[0][1] / input_TOP_L[0][2]
+        input_TOP_R = np.asarray(homography.dot(np.asarray([input_maxCol, 0, 1])))
+        input_TOP_R_actual = input_TOP_R[0][0] / input_TOP_R[0][2], input_TOP_R[0][1] / input_TOP_R[0][2]
+        input_BOT_L = np.asarray(homography.dot(np.asarray([0, input_maxRow, 1])))
+        input_BOT_L_actual = input_BOT_L[0][0] / input_BOT_L[0][2], input_BOT_L[0][1] / input_BOT_L[0][2]
+        input_BOT_R = np.asarray(homography.dot(np.asarray([input_maxCol, input_maxRow, 1])))
+        input_BOT_R_actual = input_BOT_R[0][0] / input_BOT_R[0][2], input_BOT_R[0][1] / input_BOT_R[0][2]
+
+        input_coords = []
+        input_coords.append(input_TOP_L_actual)
+        input_coords.append(input_TOP_R_actual)
+        input_coords.append(input_BOT_L_actual)
+        input_coords.append(input_BOT_R_actual)
+
+        input_smallest_x = min([x[0] for x in input_coords])
+        input_greatest_x = max([x[0] for x in input_coords])
+        input_smallest_y = min([x[1] for x in input_coords])
+        input_greatest_y = max([x[1] for x in input_coords])
+
+        # find 4 corners in new image
+        x_shape = int(input_greatest_x - input_smallest_x)
+        y_shape = int(input_greatest_y - input_smallest_y)
+
+        newImg = np.zeros((y_shape, x_shape, 3), dtype=np.uint8)
+        for rows in range(newImg.shape[0]):
+            for cols in range(newImg.shape[1]):
+                homogenous_coords = np.asarray([cols + input_smallest_x, rows + input_smallest_y, 1])
+                input_coords = np.asarray(inverted_H.dot(homogenous_coords))
+
+                # Convert out of homogenous
+                input_coords_normed = (input_coords[0][0] / input_coords[0][2], input_coords[0][1] / input_coords[0][2])
+                input_coords_actual = (int(input_coords_normed[0]),
+                                       int(input_coords_normed[1]))
+
+                if input_coords_actual[0] >= 0 and input_coords_actual[0] < inputIm.shape[1]:
+                    if input_coords_actual[1] >= 0 and input_coords_actual[1] < inputIm.shape[0]:
+                        if not np.array_equal(BACKGROUND_PIXEL, inputIm[input_coords_actual[1]][input_coords_actual[0]]):
+                            newImg[rows][cols] = inputIm[input_coords_actual[1]][input_coords_actual[0]]
+
+        ## MERGE ##
+        # find 4 corners in new image
+        x_min = min(input_smallest_x, 0)
+        x_max = max(input_greatest_x, refIm.shape[1])
+        y_min = min(input_smallest_y, 0)
+        y_max = max(input_greatest_y, refIm.shape[0])
+
+        x_shape = int(x_max - x_min)
+        y_shape = int(y_max - y_min)
+
+        stitchedImg = np.zeros((y_shape, x_shape, 3), dtype=np.uint8)
+
+        for rows in range(stitchedImg.shape[0]):
+            for cols in range(stitchedImg.shape[1]):
+                homogenous_coords = np.asarray([int(cols + x_min), int(rows + y_min), 1])
+                input_coords = np.asarray(inverted_H.dot(homogenous_coords))
+
+                # Convert out of homogenous
+                input_coords_normed = (input_coords[0][0] / input_coords[0][2], input_coords[0][1] / input_coords[0][2])
+                input_coords_actual = (int(input_coords_normed[0]),
+                                       int(input_coords_normed[1]))
+
+                if rows + (y_min) >= 0 and rows + y_min < refIm.shape[0] and cols + x_min < refIm.shape[1] and (
+                        cols + (x_min)) >= 0:
+                    stitchedImg[rows][cols] = refIm[int(rows + y_min)][int(cols + x_min)]
+                if input_coords_actual[0] >= 0 and input_coords_actual[0] < inputIm.shape[1]:
+                    if input_coords_actual[1] >= 0 and input_coords_actual[1] < inputIm.shape[0]:
+                        if not np.array_equal(BACKGROUND_PIXEL, inputIm[input_coords_actual[1]][input_coords_actual[0]]):
+                            stitchedImg[rows][cols] = image[input_coords_actual[1]][input_coords_actual[0]]
+
+        # fig, ax = plt.subplots(1, 2)
+        img_vec2 = stitchedImg
+        # ax[0].imshow(img_vec2)
+        blurred = cv2.medianBlur(stitchedImg, 15)
+        sharpened = misc.imfilter(blurred, 'sharpen')
+        # ax[1].imshow(sharpened)
+        # plt.title('Merged Shirt with blurring')
+        # plt.show()
+        return img_vec2, sharpened
+
+
+
+
 
     def initUI(self):
         self.setWindowTitle(self.title)
@@ -193,23 +360,15 @@ class SeamApp(QWidget):
         self.layout().addLayout(vbox)
         self.show()
 
-    # def getPixel(self, event):
-    #     x = event.pos().x()
-    #     y = event.pos().y()
-    #     print(x, y)
-        # return (x,y)
-    # def deleteItems(self, layout):
-    #     if layout is not None:
-    #         while layout.count():
-    #             item = layout.takeAt(0)
-    #             widget = item.widget()
-    #             if widget is not None:
-    #                 if widget not in self.referenceWidgetList:
-    #                     widget.deleteLater()
-    #                 else:
-    #                     widget.hide()
-    #             else:
-    #                 self.deleteItems(item.layout())
+    def deleteItems(self, layout):
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                        widget.hide()
+                else:
+                    self.deleteItems(item.layout())
 
 
 def pipeline():
@@ -353,4 +512,4 @@ def mapShirt(shirt):
 
 if __name__ == "__main__":
     shirt = pipeline()
-    mapShirt(shirt)
+    #mapShirt(shirt)
